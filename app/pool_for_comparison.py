78e4fa4d4cdcc25dbfd9889ff91600cda2c8f7d1
Tarik El Lel <tareklel@gmail.com>
2023-08-17 21:34:04 +0400

    added select_compare_a

--- app/pool_for_comparison.py
+++ app/pool_for_comparison.py
@@ -5,13 +5,14 @@ import json
 from app.helpers import get_keys_within_key, find_level, count_children, get_grouping
 
 
-class pool_for_comparison:
+class PoolForComparison:
     def __init__(self, conf: str):
         with open(conf, 'r') as f:
             file = yaml.safe_load(f)
         with open(file['product_tree'], 'r') as f:
             self.product_tree = json.load(f)
-        self.pair_file = file['pair_file_dest'] +  (file['product_tree'].split('.')[0]).split('/')[-1]
+        self.pair_file = file['pair_file_dest'] + \
+            (file['product_tree'].split('.')[0]).split('/')[-1]
         self.pair_file = self.pair_file + '_match.csv'
         self.schema = file['schema']
         # pivot defines where the tree splits to reveal the dimensions we're trying to compare
@@ -21,8 +22,10 @@ class pool_for_comparison:
         if not os.path.exists(self.pair_file):
 
             # create a column for each unique pivot
-            split_columns = get_keys_within_key(self.product_tree, self.pivot['pivot'])
-            split_columns = sorted([f"{x}_{self.pivot['pivot_unique']}" for x in split_columns])
+            split_columns = get_keys_within_key(
+                self.product_tree, self.pivot['pivot'])
+            split_columns = sorted(
+                [f"{x}_{self.pivot['pivot_unique']}" for x in split_columns])
             cols = self.schema[:self.schema.index(self.pivot['pivot'])]
             cols = cols + split_columns
             # create dataframe
@@ -34,13 +37,13 @@ class pool_for_comparison:
             self.pair_df = pd.read_csv(self.pair_file)
         else:
             print('pair file already exists and loaded')
-        
+
     def load_pair_file(self):
         if not self.pair_df:
             self.pair_df = pd.read_csv(self.pair_file)
         else:
             print('pair file already exists and loaded')
-    
+
     def get_unpaired(self):
         # find tree level of product
         level = find_level(self.product_tree, self.pivot['pivot'], 1) + 2
@@ -51,19 +54,36 @@ class pool_for_comparison:
         self.describe_unpaired = grouping
 
     def fetch_unpaired_group(self, group_name):
+        # Fetch all unpaired items
         self.get_unpaired()
+
+        # Start with the root of the product tree
         data = self.product_tree
+
+        # Navigate through the product tree using the group name
         for key in group_name.split('.'):
             data = data[key]
-        
+
+        # Initialize an empty dictionary for the group
         group = {}
+
+        # Populate the group dictionary with relevant data based on the pivot key
         for key in data.keys():
             group[key] = data[key][self.pivot['pivot_unique']]
-            
+
+        # Return the constructed group
         return group
-    
 
 
-    
-    
+class ComparePool:
+    def __init__(self, group, group_name):
+        self.group = group
+        self.group_name = group_name
+
+    def select_compare_a(self):
+        compare_a_key = list(self.group.keys())[0]
+        if len(self.group[compare_a_key]) > 0:
+            product_a_key = list(self.group[compare_a_key].keys())[0]
+            self.compare_a = {product_a_key: self.group[compare_a_key][product_a_key]}
+
 
--- app/tests/test_pool_comparison.py
+++ app/tests/test_pool_comparison.py
@@ -1,5 +1,5 @@
 import os
-from app.pool_for_comparison import pool_for_comparison
+from app.pool_for_comparison import PoolForComparison, ComparePool
 import unittest
 import pandas as pd
 from pandas.testing import assert_frame_equal
@@ -8,14 +8,19 @@ import json
 
 class TestSetupComparison(unittest.TestCase):
     def setUp(self):
-        self.test_obj = pool_for_comparison(
+        self.test_obj = PoolForComparison(
             conf='app/tests/resources/pool_for_comparison.yml')
+
+        with open('app/tests/resources/json/test_fetch_unpaired_group_mini.json') as f:
+            compare = json.load(f)
+
+        self.test_compare = ComparePool(
+            compare, 'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
 
     def test_create_pair_file(self):
         test_df = pd.read_csv(
             'app/tests/resources/compare/farfetch_mini_ounass_mini_match.csv')
         self.test_obj.create_pair_file()
-        print(self.test_obj.pair_df, test_df)
         assert_frame_equal(self.test_obj.pair_df, test_df, check_dtype=False)
         try:
             os.remove(
@@ -30,12 +35,22 @@ class TestSetupComparison(unittest.TestCase):
                                                                                                         'Ounass.product_name': 9}}
         self.test_obj.get_unpaired()
         self.assertEqual(self.test_obj.describe_unpaired, compare)
-    
+
     def test_fetch_unpaired_group(self):
-        asserted_pair = self.test_obj.fetch_unpaired_group('crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
+        asserted_pair = self.test_obj.fetch_unpaired_group(
+            'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
         with open('app/tests/resources/json/test_fetch_unpaired_group.json') as f:
-            compare = json.load(f) 
+            compare = json.load(f)
         self.assertEqual(asserted_pair, compare)
+
+    def test_select_compare_a(self):
+        self.test_compare.select_compare_a()
+        to_assert = {
+            "panelled-design sneakers": {
+                "text": "['panelled-design sneakers', 'FARFETCH ID: ', 'Brand style ID: ']"
+            }}
+        self.assertEqual(self.test_compare.compare_a, to_assert)
+
 
 if __name__ == '__main__':
     unittest.main()
