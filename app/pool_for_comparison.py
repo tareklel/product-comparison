14d176cc013d27943364603aa98058672df66283
Tarik El Lel <tareklel@gmail.com>
2023-08-20 19:49:33 +0400

    added select group name to Pool For Comparison

--- app/pool_for_comparison.py
+++ app/pool_for_comparison.py
@@ -5,76 +5,6 @@ import json
 from app.helpers import get_keys_within_key, find_level, count_children, get_grouping
 
 
-class PoolForComparison:
-    def __init__(self, conf: str):
-        with open(conf, 'r') as f:
-            file = yaml.safe_load(f)
-        with open(file['product_tree'], 'r') as f:
-            self.product_tree = json.load(f)
-        self.pair_file = file['pair_file_dest'] + \
-            (file['product_tree'].split('.')[0]).split('/')[-1]
-        self.pair_file = self.pair_file + '_match.csv'
-        self.schema = file['schema']
-        # pivot defines where the tree splits to reveal the dimensions we're trying to compare
-        self.pivot = file['pivot']
-
-    def create_pair_file(self):
-        if not os.path.exists(self.pair_file):
-
-            # create a column for each unique pivot
-            split_columns = get_keys_within_key(
-                self.product_tree, self.pivot['pivot'])
-            split_columns = sorted(
-                [f"{x}_{self.pivot['pivot_unique']}" for x in split_columns])
-            cols = self.schema[:self.schema.index(self.pivot['pivot'])]
-            cols = cols + split_columns
-            # create dataframe
-            df = pd.DataFrame({key: {} for key in cols})
-            # write
-            df.to_csv(self.pair_file, index=False)
-        # load pair file if it exists
-        if not hasattr(self, 'pair_df'):
-            self.pair_df = pd.read_csv(self.pair_file)
-        else:
-            print('pair file already exists and loaded')
-
-    def load_pair_file(self):
-        if not self.pair_df:
-            self.pair_df = pd.read_csv(self.pair_file)
-        else:
-            print('pair file already exists and loaded')
-
-    def get_unpaired(self):
-        # find tree level of product
-        level = find_level(self.product_tree, self.pivot['pivot'], 1) + 2
-        # get number of children
-        children_count = count_children(self.product_tree, 1, level)
-        # get number of unpaired in each level
-        grouping = get_grouping(children_count, self.pivot['pivot'])
-        self.describe_unpaired = grouping
-
-    def fetch_unpaired_group(self, group_name):
-        # Fetch all unpaired items
-        self.get_unpaired()
-
-        # Start with the root of the product tree
-        data = self.product_tree
-
-        # Navigate through the product tree using the group name
-        for key in group_name.split('.'):
-            data = data[key]
-
-        # Initialize an empty dictionary for the group
-        group = {}
-
-        # Populate the group dictionary with relevant data based on the pivot key
-        for key in data.keys():
-            group[key] = data[key][self.pivot['pivot_unique']]
-
-        # Return the constructed group
-        return group
-
-
 class ComparePool:
     def __init__(self, group, group_name):
         self.group = group
@@ -155,4 +85,88 @@ class ComparePool:
                      self.second_group_key: self.group[self.second_group_key]}
 
         return {'matched': self.matched, 'unmatched': unmatched}
+
+
+class PoolForComparison:
+    def __init__(self, conf: str):
+        with open(conf, 'r') as f:
+            file = yaml.safe_load(f)
+        with open(file['product_tree'], 'r') as f:
+            self.product_tree = json.load(f)
+        self.pair_file = file['pair_file_dest'] + \
+            (file['product_tree'].split('.')[0]).split('/')[-1]
+        self.pair_file = self.pair_file + '_match.csv'
+        self.schema = file['schema']
+        # pivot defines where the tree splits to reveal the dimensions we're trying to compare
+        self.pivot = file['pivot']
+
+    def create_pair_file(self):
+        if not os.path.exists(self.pair_file):
+
+            # create a column for each unique pivot
+            split_columns = get_keys_within_key(
+                self.product_tree, self.pivot['pivot'])
+            split_columns = sorted(
+                [f"{x}_{self.pivot['pivot_unique']}" for x in split_columns])
+            cols = self.schema[:self.schema.index(self.pivot['pivot'])]
+            cols = cols + split_columns
+            # create dataframe
+            df = pd.DataFrame({key: {} for key in cols})
+            # write
+            df.to_csv(self.pair_file, index=False)
+        # load pair file if it exists
+        if not hasattr(self, 'pair_df'):
+            self.pair_df = pd.read_csv(self.pair_file)
+        else:
+            print('pair file already exists and loaded')
+
+    def load_pair_file(self):
+        if not self.pair_df:
+            self.pair_df = pd.read_csv(self.pair_file)
+        else:
+            print('pair file already exists and loaded')
+
+    def get_unpaired(self):
+        # find tree level of product
+        level = find_level(self.product_tree, self.pivot['pivot'], 1) + 2
+        # get number of children
+        children_count = count_children(self.product_tree, 1, level)
+        # get number of unpaired in each level
+        grouping = get_grouping(children_count, self.pivot['pivot'])
+        self.describe_unpaired = grouping
+
+    def fetch_unpaired_group(self, group_name):
+        # Fetch all unpaired items
+        self.get_unpaired()
+
+        # Start with the root of the product tree
+        data = self.product_tree
+
+        # Navigate through the product tree using the group name
+        for key in group_name.split('.'):
+            data = data[key]
+
+        # Initialize an empty dictionary for the group
+        group = {}
+
+        # Populate the group dictionary with relevant data based on the pivot key
+        for key in data.keys():
+            group[key] = data[key][self.pivot['pivot_unique']]
+
+        # Return the constructed group
+        return group
     
+    # select group_name
+    # cycle through group_names
+
+    def select_group_name(self):
+        self.get_unpaired()
+        if len(list(self.describe_unpaired.keys())) > 0:
+            self.selected_group_name = list(self.describe_unpaired.keys())[0]
+
+    def start_compare_pool(self, group_name):
+        group = self.fetch_unpaired_group(group_name)
+        self.comparepool = ComparePool(group, group_name)
+
+
+
--- app/tests/test_pool_comparison.py
+++ app/tests/test_pool_comparison.py
@@ -19,32 +19,6 @@ class TestSetupComparison(unittest.TestCase):
         self.test_compare = ComparePool(
             compare, 'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
 
-    def test_create_pair_file(self):
-        test_df = pd.read_csv(
-            'app/tests/resources/compare/farfetch_mini_ounass_mini_match.csv')
-        self.test_obj.create_pair_file()
-        assert_frame_equal(self.test_obj.pair_df, test_df, check_dtype=False)
-        try:
-            os.remove(
-                'app/tests/resources/output/farfetch_mini_ounass_mini_match.csv')
-        except FileNotFoundError:
-            pass
-
-    def test_get_unpaired(self):
-        compare = {'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site': {'Farfetch.product_name': 33,
-                                                                                                     'Ounass.product_name': 7},
-                   'crawl_date.2023-06-18.country.sa.gender.women.brand.Burberry.category.shoes.site': {'Farfetch.product_name': 26,
-                                                                                                        'Ounass.product_name': 9}}
-        self.test_obj.get_unpaired()
-        self.assertEqual(self.test_obj.describe_unpaired, compare)
-
-    def test_fetch_unpaired_group(self):
-        asserted_pair = self.test_obj.fetch_unpaired_group(
-            'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
-        with open('app/tests/resources/json/test_fetch_unpaired_group.json') as f:
-            compare = json.load(f)
-        self.assertEqual(asserted_pair, compare)
-
     def test_select_compare_a(self):
         self.test_compare.select_compare_a()
         to_assert = {
@@ -150,11 +124,43 @@ class TestSetupComparison(unittest.TestCase):
         self.test_compare.select_next_a()
 
         test2 = {
-                "panelled-design sneakers": {
-                    "text": "['panelled-design sneakers', 'FARFETCH ID: ', 'Brand style ID: ']"
-                }
+            "panelled-design sneakers": {
+                "text": "['panelled-design sneakers', 'FARFETCH ID: ', 'Brand style ID: ']"
             }
+        }
         self.assertEqual(self.test_compare.compare_a, test2)
+
+    def test_create_pair_file(self):
+        test_df = pd.read_csv(
+            'app/tests/resources/compare/farfetch_mini_ounass_mini_match.csv')
+        self.test_obj.create_pair_file()
+        assert_frame_equal(self.test_obj.pair_df, test_df, check_dtype=False)
+        try:
+            os.remove(
+                'app/tests/resources/output/farfetch_mini_ounass_mini_match.csv')
+        except FileNotFoundError:
+            pass
+
+    def test_get_unpaired(self):
+        compare = {'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site': {'Farfetch.product_name': 33,
+                                                                                                     'Ounass.product_name': 7},
+                   'crawl_date.2023-06-18.country.sa.gender.women.brand.Burberry.category.shoes.site': {'Farfetch.product_name': 26,
+                                                                                                        'Ounass.product_name': 9}}
+        self.test_obj.get_unpaired()
+        self.assertEqual(self.test_obj.describe_unpaired, compare)
+
+    def test_fetch_unpaired_group(self):
+        asserted_pair = self.test_obj.fetch_unpaired_group(
+            'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
+        with open('app/tests/resources/json/test_fetch_unpaired_group.json') as f:
+            compare = json.load(f)
+        self.assertEqual(asserted_pair, compare)
+
+    def test_select_group_name(self):
+
+        self.test_obj.select_group_name()
+        self.assertEqual(self.test_obj.selected_group_name,
+                        'crawl_date.2023-06-18.country.sa.gender.women.brand.KENZO.category.shoes.site')
 
 
 if __name__ == '__main__':
