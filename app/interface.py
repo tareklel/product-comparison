7728d984debe534c7c2ac4fc8e271fb2c410cc75
Tarik El Lel <tareklel@gmail.com>
2023-09-19 21:56:06 +0400

    added match_products and save and close function to gui

--- app/interface.py
+++ app/interface.py
@@ -56,20 +56,38 @@ class GuiPoolForComparison(tk.Toplevel):
             self, text="Select", command=lambda: self.select_group(selected_value.get()))
         select_button.pack()
 
+        print_matched = tk.Button(
+            self, text="Print Matched", command=self.print_matched)
+        print_matched.pack()
+
+        consolidate = tk.Button(
+            self, text="Consolidate Matched", command=self.consolidate_matched)
+        consolidate.pack()
+
     def select_group(self, selected_value):
+        if any(selected_value in d for d in self.pool.matched):
+            return
         if self.pool.file['images_dest']:
             self.pool.start_compare_pool(
                 selected_value, self.pool.file['images_dest'])
         else:
             self.pool.start_compare_pool(selected_value)
 
-        GuiComparePool(self.pool.comparepool)
+        GuiComparePool(self.pool)
+    
+    def print_matched(self):
+        print(self.pool.matched)
+    
+    def consolidate_matched(self):
+        # self.pool.consolidate_matched()
+        return
 
 
 class GuiComparePool(tk.Toplevel):
-    def __init__(self, obj: ComparePool):
+    def __init__(self, pool: ComparePool):
         tk.Toplevel.__init__(self)
-        self.obj = obj
+        self.pool = pool
+        self.obj = self.pool.comparepool
         self.title('Compare Pool')
         self.geometry("1300x900")
         self.obj.select_compare_a()
@@ -77,25 +95,26 @@ class GuiComparePool(tk.Toplevel):
 
         # Create a canvas and a vertical scrollbar
         self.canvas = tk.Canvas(self)
-        self.main_scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.main_scrollbar = tk.Scrollbar(
+            self, orient="vertical", command=self.canvas.yview)
         self.main_scrollbar.pack(side="left", fill="y")
 
         # Configure the canvas
         self.canvas.pack(side="right", fill="both", expand=True)
         self.canvas.configure(yscrollcommand=self.main_scrollbar.set)
-        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(
+            scrollregion=self.canvas.bbox("all")))
         # Create a frame inside the canvas and add it to the canvas window
         self.frame = tk.Frame(self.canvas)
-        self.canvas.create_window((0,0), window=self.frame, anchor="nw")
+        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
 
         self.group_name = tk.Label(self.frame, text=self.obj.group_name)
         self.group_name.grid(sticky='w', row=0, column=1)
 
-
         self.next_button = tk.Button(
-            self.frame, text='Next Compare Group', command=lambda: '')
+            self.frame, text='Next Compare Product', command=self.next_compare_product)
         self.save_close_button = tk.Button(
-            self.frame, text='Save and Close', command=lambda: '')
+            self.frame, text='Save and Close', command=self.save_close)
         self.next_button.grid(sticky='w', row=0, column=3)
         self.save_close_button.grid(sticky='w', row=0, column=4)
 
@@ -129,7 +148,7 @@ class GuiComparePool(tk.Toplevel):
         self.pool_name_b.grid(sticky='w', row=7, column=1)
 
         # Loop through the dictionary and create radio buttons
-        radio_selected_key = tk.StringVar()
+        self.radio_selected_key = tk.StringVar()
         # Frame for radio buttons (using pack)
 
         # Create a canvas and a vertical scrollbar
@@ -140,7 +159,8 @@ class GuiComparePool(tk.Toplevel):
             self.frame, orient="vertical", command=self.canvas_compare2.yview)
 
         # Configure the canvas
-        self.canvas_compare2.config(yscrollcommand=self.compare_2_scrollbar.set)
+        self.canvas_compare2.config(
+            yscrollcommand=self.compare_2_scrollbar.set)
         self.compare_2_scrollbar.grid(sticky='ns', row=8, column=0)
 
         # Frame inside canvas with radio
@@ -150,15 +170,17 @@ class GuiComparePool(tk.Toplevel):
         for i, (key, value) in enumerate(self.obj.product_b_list.items()):
             tk.Radiobutton(self.frame1,
                            text=key,
-                           variable=radio_selected_key,
+                           variable=self.radio_selected_key,
                            value=key).grid(sticky='ns', row=i * 2, column=0)
-            tk.Label(self.frame1, text=value, wraplength=600).grid(sticky='ns', row=i * 2 + 1, column=0)
+            tk.Label(self.frame1, text=value, wraplength=600).grid(
+                sticky='ns', row=i * 2 + 1, column=0)
             # get image for compare a
             image_b = self.get_images(
                 self.obj.second_group_key, key)
             if image_b:
                 self.image2_dict[key] = image_b
-                self.imageb_label = tk.Label(self.frame1, image=self.image2_dict[key])
+                self.imageb_label = tk.Label(
+                    self.frame1, image=self.image2_dict[key])
                 self.imageb_label.grid(sticky='ns', row=i * 2 + 1, column=1)
 
         self.canvas_compare2.create_window(
@@ -166,11 +188,12 @@ class GuiComparePool(tk.Toplevel):
 
         # Update the scrollregion after configuring the interior frame
         self.frame1.update_idletasks()
-        self.canvas_compare2.config(scrollregion=self.canvas_compare2.bbox("all"))
+        self.canvas_compare2.config(
+            scrollregion=self.canvas_compare2.bbox("all"))
 
         # Add a button to print the selected value
         self.radio_select = tk.Button(
-            self.frame, text="Match products", command='')
+            self.frame, text="Match products", command=lambda: self.match_products(self.radio_selected_key.get()))
 
         self.radio_select.grid(sticky='w', row=0, column=2)
 
@@ -193,6 +216,48 @@ class GuiComparePool(tk.Toplevel):
         image = image.resize((100, 100))
         photo = ImageTk.PhotoImage(image)
         return photo
+
+    def next_compare_product(self):
+        """Change compare product a"""
+        self.obj.select_next_a()
+        self.compare_a_name.config(text=next(iter(self.obj.compare_a)))
+        self.compare_a_description.config(
+            text=self.obj.compare_a[next(iter(self.obj.compare_a))], wraplength=600)
+
+        # get image for compare a
+        self.image_a = self.get_images(
+            self.obj.first_group_key, next(iter(self.obj.compare_a)))
+        if self.image_a:
+            self.image1_label.config(image=self.image_a)
+    
+    def clear_frame(self, frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
+    
+    def match_products(self, product):
+        self.obj.select_match(product)
+        self.clear_frame(self.frame1)
+        self.image2_dict = {}
+        self.next_compare_product()
+        for i, (key, value) in enumerate(self.obj.product_b_list.items()):
+            tk.Radiobutton(self.frame1,
+                           text=key,
+                           variable=self.radio_selected_key,
+                           value=key).grid(sticky='ns', row=i * 2, column=0)
+            tk.Label(self.frame1, text=value, wraplength=600).grid(
+                sticky='ns', row=i * 2 + 1, column=0)
+            # get image for compare a
+            image_b = self.get_images(
+                self.obj.second_group_key, key)
+            if image_b:
+                self.image2_dict[key] = image_b
+                self.imageb_label = tk.Label(
+                    self.frame1, image=self.image2_dict[key])
+                self.imageb_label.grid(sticky='ns', row=i * 2 + 1, column=1)
+    
+    def save_close(self):
+        self.pool.update_matched()
+        self.destroy()
 
 
 # Create Tkinter window and the app instance
